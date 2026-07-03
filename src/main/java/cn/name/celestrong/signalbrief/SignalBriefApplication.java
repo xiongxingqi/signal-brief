6766e937f56c0e11ba8f0ea7a830a8bfcf5554b2
celestrong <1269504317@qq.com>
2026-07-03 20:12:41 +0800

    init: 初始化项目

---
+++ src/main/java/cn/name/celestrong/signalbrief/SignalBriefApplication.java
@@ -0,0 +1,13 @@
+package cn.name.celestrong.signalbrief;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignalBriefApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SignalBriefApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/name/celestrong/signalbrief/SignalBriefApplicationIT.java
@@ -0,0 +1,13 @@
+package cn.name.celestrong.signalbrief;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignalBriefApplicationIT {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
